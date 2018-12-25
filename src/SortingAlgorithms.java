62c94514b39a3b11f326ffdfbdc8bad9c7925eb3
Sadok Dalin <skogsfaktor@gmail.com>
2018-12-25 03:26:08 +0100

    More skeleton

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,8 @@
+public class SortingAlgorithms {
+    public static int[] insertionSort(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+
+        }
+        return array;
+    }
+}
---
+++ src/Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+        int testArray[] = {4,3,2,1};
+    }
+}

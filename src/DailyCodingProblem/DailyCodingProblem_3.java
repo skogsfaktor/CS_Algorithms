4beec61599b823f9554f5f027bcc79f00ff1ea8e
Sadok Dalin <skogsfaktor@gmail.com>
2019-03-12 23:24:46 +0100

    CHC och jag i 3:an

--- src/DailyCodingProblem/DailyCodingProblem_3.java
+++ src/DailyCodingProblem/DailyCodingProblem_3.java
@@ -1,4 +1,5 @@
-/*Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+/*
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
 For example, given the following Node class
 class Node:
         def __init__(self, val, left=None, right=None):
@@ -8,8 +9,8 @@ class Node:
 
         The following test should pass:
         node = Node('root', Node('left', Node('left.left')), Node('right'))
-        assert deserialize(serialize(node)).left.left.val == 'left.left'*/
-
+        assert deserialize(serialize(node)).left.left.val == 'left.left'
+*/
 package DailyCodingProblem;
 
 public class DailyCodingProblem_3 {
@@ -17,6 +18,10 @@ public class DailyCodingProblem_3 {
         String val;
         Node left;
         Node right;
+
+        public Node() {
+            
+        }
 
         public Node(String val) {
             this.val = val;
@@ -45,16 +50,59 @@ public class DailyCodingProblem_3 {
         }
 
         public Node deserialize(String serialized) {
-            for(int i = 0; i < serialized.length(); i++) {
-                if(serialized.charAt(i) == '[') {
-                    StringBuilder nodeVal = new StringBuilder();
-                    for(int j = i++; j < i; j++) {
+            //Input: [root, [left, [left.left, null, null], null], [right, null, null]]
+            //if(left = "null") -> return null
+            //new Node(val, deserialize(left), deserialize(right))
+            //[val, l, r]
 
-                    }
-                }
+            //Get val
+            int valueEnd = serialized.indexOf(',');
+            String value = serialized.substring(1, valueEnd);
+            
+            Node left, right;
+            //Index of opening bracket [ or n 
+            int leftStart = valueEnd+2;
+            int leftEnd;
+            //Get left, count brackets, check null
+
+            if(serialized.charAt(leftStart) == 'n') {
+                //null
+                leftEnd = valueEnd + 6;
+                left = null;
+            } else {
+                //node 
+                leftEnd = leftStart + treeEndPos(serialized.substring(leftStart));
+                left = deserialize(serialized.substring(leftStart, leftEnd+1));
             }
 
-            return new Node("ass");
+            int rightStart = leftEnd+2;
+            //Index of opening bracket [ or n 
+            int rightEnd;
+            //Get left, count brackets, check null
+            if(serialized.charAt(rightStart) == 'n') {
+                //null
+                right = null;
+            } else {
+                //node 
+                rightEnd = rightStart + treeEndPos(serialized.substring(rightStart));
+                right = deserialize(serialized.substring(rightStart, rightEnd+1));
+            }
+
+            return new Node(value, left, right);
+        }
+
+        private int treeEndPos(String tree) {
+            int count = 1;
+            int pos = 1;
+            while (count != 0){
+                if (tree.charAt(pos) == '[') {
+                    count++;
+                } else if (tree.charAt(pos) == ']') {
+                    count--;
+                }
+                pos++;
+            }
+            return pos;
         }
     }
 
@@ -63,7 +111,9 @@ public class DailyCodingProblem_3 {
                 new DailyCodingProblem_3.Node("left", new DailyCodingProblem_3.Node("left.left")),
                 new DailyCodingProblem_3.Node("right"));
 
-        System.out.println(tree.right.val);
+        //System.out.println(tree.right.val);
         System.out.println(tree.serialize(tree));
+        System.out.println(tree.deserialize(tree.serialize(tree)).left.left.val);
+        System.out.println(tree.deserialize(tree.serialize(tree)).right.val);
     }
 }
